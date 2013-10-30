c00e6ff07232bca51ddf232a4ca1db796c5eaeb2
Mota-Torre <Mota-Torre@Mota-Torre-Pc.home>
2013-10-30 13:02:44 +0000

    Declaração da Classe. Criação do Layout para validação.

---
+++ src/com/feup/passenger/Validate.java
@@ -0,0 +1,37 @@
+package com.feup.passenger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.feup.passenger.R;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class Validate extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.validate);
+		
+	}
+	
+}
