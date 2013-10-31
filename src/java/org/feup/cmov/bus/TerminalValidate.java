b66cf8a3456dc83b2e74890696039440f6de8dee
Ricardo <ei09045@fe.up.pt>
2013-10-31 04:41:39 +0000

    Added Online Validation Function

---
+++ src/java/org/feup/cmov/bus/TerminalValidate.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.feup.cmov.bus;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class TerminalValidate {
+    public int UserId;
+    public String Type;
+    public int BusId;
+    
+    public TerminalValidate()
+    {
+        UserId=-1;
+        BusId=-1;
+        Type="";
+    }
+    public TerminalValidate(int UserId, String Type, int BusId)
+    {
+        this.UserId=UserId;
+        this.Type=Type;
+        this.BusId=BusId;
+    }
+}
--- src/java/org/feup/cmov/bus/TicketResource.java
+++ src/java/org/feup/cmov/bus/TicketResource.java
@@ -40,7 +40,7 @@ public class TicketResource {
             ResultSet rs = stmt.executeQuery(query);
 
             while ( rs.next() ) {
-                Ticket newTicket = new Ticket(rs.getInt("APP.TICKETID"), rs.getString("TYPE"), rs.getInt("USERID"));
+                Ticket newTicket = new Ticket(rs.getInt("TICKETID"), rs.getString("TYPE"), rs.getInt("USERID"));
                 tickets.Tickets.add(newTicket);
                 System.out.println(newTicket.Id);
             }
@@ -70,7 +70,7 @@ public class TicketResource {
             SimpleDateFormat format = new SimpleDateFormat("yyyy-M-dd hh:mm:ss");
             System.out.println(format.format(currentDate.getTime()));
             
-            String query = "SELECT * FROM APP.TICKETS, APP.USERTICKETS, APP.BUSTICKET WHERE APP.TICKETS.ID=APP.USERTICKETS.TICKETID AND APP.TICKETS.ID=APP.USERTICKETS.TICKETID AND APP.USERTICKETS.TICKETID=APP.BUSTICKET.TICKETID AND APP.BUSTICKET.VALIDATIONDATE >= '"+format.format(currentDate.getTime())+"' AND APP.USERTICKETS.STATUS=1";
+            String query = "SELECT * FROM APP.TICKETS, APP.USERTICKETS, APP.BUSTICKET WHERE APP.TICKETS.ID=APP.USERTICKETS.TICKETID AND APP.USERTICKETS.TICKETID=APP.BUSTICKET.TICKETID AND APP.BUSTICKET.VALIDATIONDATE >= '"+format.format(currentDate.getTime())+"' AND APP.USERTICKETS.STATUS=1";
             ResultSet rs = stmt.executeQuery(query);
             while ( rs.next() ) {
                 String date= rs.getString("VALIDATIONDATE");
@@ -85,6 +85,99 @@ public class TicketResource {
         }
         System.out.println("ok");
         return tickets;
+    }
+    
+    
+    @POST
+    @Path("TerminalValidate/{key}")
+    @Produces("text/plain")
+    @Consumes("application/json")
+    public String terminalValidate(@PathParam("key") String key, TerminalValidate tv) {
+        int index = 0;
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            String url = "jdbc:derby://localhost:1527/BusDB";
+            Connection conn = DriverManager.getConnection(url,"test","test");
+            Statement stmt = conn.createStatement();
+            Ticket ticket = new Ticket();
+           
+            Calendar currentDate = Calendar.getInstance(); //Get the current date
+            currentDate.add(Calendar.MINUTE, -15);
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-M-dd hh:mm:ss");
+            System.out.println(format.format(currentDate.getTime()));
+            
+            System.out.println("Check if there is already a validated ticket");
+            
+            String query = "SELECT * FROM APP.TICKETS, APP.BUSTICKET, APP.USERTICKETS WHERE APP.TICKETS.ID=APP.BUSTICKET.TICKETID AND APP.TICKETS.ID=APP.USERTICKETS.TICKETID AND APP.TICKETS.TYPE='T1' AND APP.BUSTICKET.VALIDATIONDATE >= '"+format.format(currentDate.getTime())+"' AND APP.USERTICKETS.USERID="+ tv.UserId;
+            ResultSet rs = stmt.executeQuery(query);
+            if ( rs.next() ) {
+                return "-3";
+            }
+            
+            currentDate = Calendar.getInstance(); //Get the current date
+            currentDate.add(Calendar.MINUTE, -30);
+            System.out.println(format.format(currentDate.getTime()));
+            
+            query = "SELECT * FROM APP.TICKETS, APP.BUSTICKET, APP.USERTICKETS WHERE APP.TICKETS.ID=APP.BUSTICKET.TICKETID AND APP.TICKETS.ID=APP.USERTICKETS.TICKETID AND APP.TICKETS.TYPE='T2' AND APP.BUSTICKET.VALIDATIONDATE >= '"+format.format(currentDate.getTime())+"' AND APP.USERTICKETS.USERID="+ tv.UserId;
+            rs = stmt.executeQuery(query);
+            if ( rs.next() ) {
+                return "-3";
+            }
+            
+            currentDate = Calendar.getInstance(); //Get the current date
+            currentDate.add(Calendar.HOUR_OF_DAY, -1);
+            System.out.println(format.format(currentDate.getTime()));
+            
+            query = "SELECT * FROM APP.TICKETS, APP.BUSTICKET, APP.USERTICKETS WHERE APP.TICKETS.ID=APP.BUSTICKET.TICKETID AND APP.TICKETS.ID=APP.USERTICKETS.TICKETID AND APP.TICKETS.TYPE='T3' AND APP.BUSTICKET.VALIDATIONDATE >= '"+format.format(currentDate.getTime())+"' AND APP.USERTICKETS.USERID="+ tv.UserId;
+            rs = stmt.executeQuery(query);
+            if ( rs.next() ) {
+                return "-3";
+            }
+            
+            System.out.println("Checking Available Tickets");
+            
+            query = "SELECT * FROM APP.TICKETS, APP.USERTICKETS WHERE APP.TICKETS.ID=APP.USERTICKETS.TICKETID AND APP.USERTICKETS.STATUS=0 AND APP.USERTICKETS.USERID="+tv.UserId+" AND APP.TICKETS.TYPE='"+tv.Type+"'";
+            rs = stmt.executeQuery(query);
+            if ( rs.next() ) {
+                ticket.UserId = rs.getInt("USERID");
+                ticket.Id= rs.getInt("TICKETID");
+                ticket.Type = rs.getString("TYPE");
+                ticket.State=1;
+            }
+            else
+            {
+                return "-2";
+            }
+            System.out.println("Updating Usertickets");
+            
+            query = "UPDATE APP.USERTICKETS SET STATUS=1 WHERE TICKETID="+ticket.Id;
+            stmt.executeUpdate(query);
+            
+            
+            query = "SELECT MAX(Id) FROM APP.BUSTICKET";
+            rs = stmt.executeQuery(query);
+            if (rs.next()) {
+                index = rs.getInt(1) + 1;
+            }
+            
+            currentDate = Calendar.getInstance(); //Get the current date
+            System.out.println("Inserting in BusTicket");
+            query = "INSERT INTO APP.BUSTICKET VALUES(" + index + ", " + ticket.Id + ", " + tv.BusId + ", '" + format.format(currentDate.getTime())+ "')";
+            stmt.executeUpdate(query);
+            
+            query = "SELECT COUNT(TICKETID) AS COUNTRESULT FROM APP.TICKETS, APP.USERTICKETS WHERE APP.TICKETS.ID=APP.USERTICKETS.TICKETID AND APP.USERTICKETS.STATUS=0 AND APP.USERTICKETS.USERID="+tv.UserId+" AND APP.TICKETS.TYPE='"+tv.Type+"'";
+            rs = stmt.executeQuery(query);
+            if (rs.next()) {
+                index = rs.getInt("COUNTRESULT");
+            }
+            
+            conn.close();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            return "-1";
+        }
+        System.out.println("ok");
+        return Integer.toString(index);
     }
     
     
